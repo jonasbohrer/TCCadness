0f4805faba8c75333614f762cf0a42e59e6bbb01
Jonas Bohrer <jonas.bohrer@hp.com>
2019-02-28 19:32:37 -0300

    Trying imagenet

--- experiments/imagenet/training.py
+++ experiments/imagenet/training.py
@@ -1,74 +1,137 @@
-import warnings
-warnings.simplefilter('ignore')
-
-import imp
+import keras, os, sys
 import matplotlib.pyplot as plot
-import numpy as np
-import os
+from keras.preprocessing.image import ImageDataGenerator
 
-import keras
-import keras.backend
-import keras.layers
-import keras.models
-import keras.utils
+input_shape = (200, 200, 3)
+
+model = keras.models.Sequential([
+            keras.layers.Conv2D(48, (3, 3), activation="relu", input_shape=input_shape),
+            keras.layers.MaxPooling2D((2, 2)),
+            keras.layers.Conv2D(32, (3, 3), activation="relu"),
+            keras.layers.MaxPooling2D((2, 2)),
+            keras.layers.Conv2D(24, (3, 3), activation="relu"),
+            keras.layers.MaxPooling2D((2, 2)),
+            keras.layers.Flatten(),
+            keras.layers.Dense(128, activation="relu"),
+            keras.layers.Dense(16, activation="softmax"),
+            ])
+model.compile(loss="sparse_categorical_crossentropy", optimizer=keras.optimizers.Adam(lr=0.005), metrics=["accuracy"])
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+train_generator = train_datagen.flow_from_directory(
+        'LEGO brick images/train/',
+        target_size=(200, 200),
+        batch_size=32,
+        class_mode='binary',
+        seed=1)
+
+#train_generator[batch_n = total/batch_size][0=image][x][y][channel]
+#train_generator[batch_n = total/batch_size][1=class][indv; size = batch_size]
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+validation_generator = test_datagen.flow_from_directory(
+        'LEGO brick images/valid/',
+        target_size=(200, 200),
+        batch_size=32,
+        class_mode='binary')
+
+#train_generator[batch_n = total/batch_size][0=image][x][y][channel]
+#train_generator[batch_n = total/batch_size][1=class][indv; size = batch_size]
+#print(len(validation_generator[0][0][0]))
+
+"""model.fit_generator(
+        train_generator,
+        steps_per_epoch=200,
+        epochs=1,
+        validation_data=validation_generator,
+        validation_steps=5)"""
+
+modelfilename = sys.argv[1] if sys.argv[1] != None else "test_model"
+checkpoints = 1
+epochs = 50
+steps_per_epoch = 50
+
+#model.load_weights(models_dir+modelname)
+
+if not os.path.exists(os.path.dirname('models/'+modelfilename+"/")):
+    try:
+        os.makedirs(os.path.dirname('models/'+modelfilename+"/"))
+    except OSError as exc: # Guard against race condition
+        if exc.errno != errno.EEXIST:
+            raise
+
+with open('models/'+modelfilename+"/summary.txt", "w") as text_file:
+    model.summary(print_fn=lambda x: text_file.write(x + '\n'))
+    text_file.write("\nepochs: {}\nsteps_per_epoch: {}\ncheckpoints: {}".format(epochs, steps_per_epoch, checkpoints))
+
+    n = 0
+
+    while n < epochs:
+        print("\nEpoch {}:".format(n))
+        history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=steps_per_epoch,
+        epochs=checkpoints,
+        validation_data=validation_generator,
+        validation_steps=int(steps_per_epoch/10))
+        n += checkpoints
+
+        #scores = model.evaluate(data[2], data[3], batch_size=batch_size)
+        #print("Scores on test set for run {}: loss/accuracy={}".format(n, tuple(scores)))
+        model.save('models/'+modelfilename+'/checkpoint_{}.h5'.format(n))
+        text_file.write("\nEpoch {}: {}".format(n, history.history))
+
+        #No need to keep after 0.95, abort
+        if history.history["acc"][-1] >= 0.95:
+            print('Achieved 95% acc')
+            n = epochs
+
+        """if n+checkpoints > epochs:
+            checkpoints = epochs - n"""
+
+
+
+"""image = validation_generator[0][0][2:3]
+c
+plot.imshow(image)
+plot.show()
+
+#####################################
 
 import innvestigate
+import innvestigate.utils.visualizations as ivis
 import innvestigate.utils as iutils
 
-epochs = 20
-batch_size = 128
-checkpoints = 1 #Number of epochs between each checkpoint
-train_size = 20000
-test_size = 2000
+methods = [("lrp.sequential_preset_a_flat",    {"epsilon": 1}, ivis.heatmap,    "LRP-PresetAFlat")]
+method = methods[0]
 
-# Use utility libraries to focus on relevant iNNvestigate routines.
-eutils = imp.load_source("utils", "../utils/utils.py")
-imgnetutils = imp.load_source("utils_imagenet", "../utils/utils_imagenet.py")
+model_wo_sm = iutils.keras.graph.model_wo_softmax(model)
+analyzer = innvestigate.create_analyzer(method[0], model_wo_sm, **method[1])#, neuron_selection_mode="index")
+analysis = analyzer.analyze(image)
+processed_analysis = iutils.postprocess_images(analysis.copy())
+processed_analysis = method[2](processed_analysis)
 
-exit(0)
+print (model.predict(image), model.predict_classes(image))
 
-# Load data
-images, label_to_class_name = eutils.get_imagenet_data(net["image_shape"][0])
+a, b = processed_analysis.max(), processed_analysis.min()
+c, d = 0, 1
+
+def preprocessing(X):
+        # shift original data to [0, b-a] (and copy)
+        X = X - a
+        # scale to new range gap [0, d-c]
+        X /= (b-a)
+        X *= (d-c)
+        # shift to desired output range
+        X += c
+        return X
 
 
-# returns x_train, y_train, x_test, y_test as numpy.ndarray
-data_not_preprocessed = mnistutils.fetch_data(train_size, test_size)
-
-# Create preprocessing functions
-input_range = [-1, 1]
-preprocess, revert_preprocessing = mnistutils.create_preprocessing_f(data_not_preprocessed[0], input_range)
-
-# Preprocess data
-data = (
-    preprocess(data_not_preprocessed[0]), keras.utils.to_categorical(data_not_preprocessed[1], 10),
-    preprocess(data_not_preprocessed[2]), keras.utils.to_categorical(data_not_preprocessed[3], 10),
-)
-
-if keras.backend.image_data_format == "channels_first":
-    input_shape = (1, 28, 28)
-else:
-    input_shape = (28, 28, 1)
-    
-model = keras.models.Sequential([
-    keras.layers.Conv2D(20, (3, 3), activation="relu", input_shape=input_shape),
-    keras.layers.Conv2D(40, (3, 3), activation="relu"),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Flatten(),
-    keras.layers.Dense(512, activation="relu"),
-    keras.layers.Dense(10, activation="softmax"),
-])
-
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-
-n = 0
-
-while n < epochs:
-    model.fit(data[0], data[1], epochs=checkpoints, batch_size=batch_size)
-    n +=checkpoints
-
-    scores = model.evaluate(data[2], data[3], batch_size=batch_size)
-    print("Scores on test set for run {}: loss/accuracy={}".format(n, tuple(scores)))
-    model.save('models/test_model{}.h5'.format(n))
-
-    if n+checkpoints > epochs:
-        checkpoints = epochs - n
+plot.imshow(preprocessing(processed_analysis).squeeze(), cmap='seismic', interpolation='nearest')
+plot.show()"""
--- experiments/imagenet/visualization.py
+++ experiments/imagenet/visualization.py
@@ -2,10 +2,12 @@ import warnings
 warnings.simplefilter('ignore')
 
 import imp
+import matplotlib as mpl
 import matplotlib.pyplot as plot
 import numpy as np
-import os, copy
+import os, copy, sys
 import imageio
+import collections
 
 import keras
 import keras.backend
@@ -15,94 +17,284 @@ import keras.utils
 
 import innvestigate
 import innvestigate.utils as iutils
+import innvestigate.utils.visualizations as ivis 
+from PIL import Image
+from keras.preprocessing.image import ImageDataGenerator
 
-# Use utility libraries to focus on relevant iNNvestigate routines.
-mnistutils = imp.load_source("utils_mnist", "../utils/utils_mnist.py")
+def join_images(images):
+    #images = map(Image.open, ['Test1.jpg', 'Test2.jpg', 'Test3.jpg'])
+    widths, heights = zip(*(i.size for i in images))
 
-# Load data
-# returns x_train, y_train, x_test, y_test as numpy.ndarray
-data_not_preprocessed = mnistutils.fetch_data()
+    total_height = sum(heights)
+    max_width = max(widths)
 
-# Create preprocessing functions
-input_range = [-1, 1]
-preprocess, revert_preprocessing = mnistutils.create_preprocessing_f(data_not_preprocessed[0], input_range)
+    new_im = Image.new('RGB', (max_width, total_height))
 
-# Preprocess data
-data = (
-    preprocess(data_not_preprocessed[0]), keras.utils.to_categorical(data_not_preprocessed[1], 10),
-    preprocess(data_not_preprocessed[2]), keras.utils.to_categorical(data_not_preprocessed[3], 10),
-)
+    y_offset = 0
+    for im in images:
+        new_im.paste(im, (0, y_offset))
+        y_offset += im.size[1]
+        
+    return new_im
 
-if keras.backend.image_data_format == "channels_first":
-    input_shape = (1, 28, 28)
-else:
-    input_shape = (28, 28, 1)
+def concat_images(imga, imgb, orientation='horizontal'):
+    """
+    Combines two color image ndarrays side-by-side.
+    """
+    if orientation == 'horizontal':
+        ha,wa = imga.shape[1:3]
+        hb,wb = imgb.shape[1:3]
+        max_height = np.max([ha, hb])
+        total_width = wa+wb
+        new_img = np.zeros(shape=(1, max_height, total_width, 3))
+        new_img[0,:ha,:wa]=imga
+        new_img[0,:hb,wa:wa+wb]=imgb
+        return new_img
+    elif orientation == 'vertical':
+        ha,wa = imga.shape[1:3]
+        hb,wb = imgb.shape[1:3]
+        max_width = np.max([wa, wb])
+        total_height = ha+hb
+        new_img = np.zeros(shape=(1, total_height, max_width, 3))
+        new_img[0,:ha,:wa]=imga
+        new_img[0,:hb,wa:wa+wb]=imgb
+        return new_img
+    else:
+        return None
 
-model = keras.models.Sequential([
-    keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=input_shape),
-    keras.layers.Conv2D(64, (3, 3), activation="relu"),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Flatten(),
-    keras.layers.Dense(512, activation="relu"),
-    keras.layers.Dense(10, activation="softmax"),
-])
+def concat_n_images(images, orientation='horizontal'):
+    """
+    Combines N color images from a list of image paths.
+    """
+    output = None
+    for i, img in enumerate(images):
+        if i==0:
+            output = img
+        else:
+            output = concat_images(output, img, orientation)
+    return output
 
-methods = [ ("lrp.z",                           {},             'imgnetutils.heatmap',    "LRP-Z"),
-            ("lrp.epsilon",                     {"epsilon": 1}, 'imgnetutils.heatmap',    "LRP-Epsilon"),
-            ("lrp.sequential_preset_a_flat",    {"epsilon": 1}, 'imgnetutils.heatmap',    "LRP-PresetAFlat"),
-            ("lrp.sequential_preset_b_flat",    {"epsilon": 1}, 'imgnetutils.heatmap',    "LRP-PresetBFlat")]
-method = methods[1]
+def generate_gifs(i, figs_dir, model, image):
 
-models_dir = 'models/'
-models = []
-for file_name in os.listdir(models_dir):
-    if file_name.endswith('.h5'):
-        print (file_name)
-        models.append(file_name)
-
-images = [
-    data[2][7:8],
-    data[2][80:81],
-    data[2][90:91]
-]
-
-# Choosing a test image for the relevance test:
-i = 0
-for image in images:
-    plot.imshow(image.squeeze(), cmap='gray', interpolation='nearest')
-    plot.savefig("models/figs/original"+str(i)+".png")
-
-    # Generate images for every model checkpoint
-    for modelname in models:
-        model.load_weights("models/"+modelname)
-
-        # Stripping the softmax activation from the model
-        model_wo_sm = iutils.keras.graph.model_wo_softmax(model)
-
-        analyzer = innvestigate.create_analyzer(method[0], model_wo_sm, **method[1])
-        analysis = analyzer.analyze(image)
-        plot.imshow(analysis.squeeze(), cmap='seismic', interpolation='nearest')
-        plot.savefig("models/figs/fig"+str(i)+"_"+modelname.replace(".h5", ".png"))
-
-    png_dir = 'models/figs/'
     files = []
-    files = [imageio.imread(png_dir+"original"+str(i)+".png")]*5
+    #files = [imageio.imread(figs_dir+'fig'+str(i)+"_original.png")]*5
     file_paths = []
+    #print (os.listdir(figs_dir))
     try:
-        for file_name in os.listdir(png_dir):
-            if file_name.endswith('.png') and file_name.startswith('fig'+str(i)):
-                file_path = os.path.join(png_dir, file_name)
+        for file_name in os.listdir(figs_dir):
+            if file_name.endswith('.png') and file_name.startswith('fig'+str(i)+'_checkpoint'):
+                file_path = os.path.join(figs_dir, file_name)
                 file_paths.append(file_path)
-        file_paths = sorted(file_paths, key=lambda x: (len(x), str.lower(x)))
+        file_paths = sorted(file_paths, key=lambda x: (len(x.split("_pred")[0]), str.lower(x)))
         for file_path in file_paths:
-            print (file_path)
             files.append(imageio.imread(file_path))
         for n in range(1,10):
             files.append(files[-1])
-        imageio.mimsave('models/figs/movie'+str(i)+'.gif', files)
+        print('generated '+figs_dir+'movie'+str(i)+'.gif')
+        imageio.mimsave(figs_dir+'movie'+str(i)+"_pred_"+str(model.predict_classes(image))+str(max(model.predict(image)[0]))+'.gif', files)
     except:
         pass
+
+def join_gifs(figs_dir, models, method):
+
+    movie = []
+    for modelname in models:
+        file_paths = []
+        files = []
+        for file_name in os.listdir(figs_dir):
+            if file_name.endswith("].png") and file_name.startswith('fig') and (modelname.replace(".h5", "_") in file_name):
+                file_path = os.path.join(figs_dir, file_name)
+                file_paths.append(file_path)
+        file_paths = sorted(file_paths, key=lambda x: (len(x.split("_pred")[0]), str.lower(x)))
+        for file_path in file_paths:
+            files.append(Image.open(file_path))
+        join_images(files).save(figs_dir+'movie_'+modelname.replace(".h5", ".png"))
+
+    file_paths = []
+    files = []
+    for file_name in os.listdir(figs_dir):
+        if file_name.endswith(".png") and file_name.startswith('movie_'):
+            file_path = os.path.join(figs_dir, file_name)
+            file_paths.append(file_path)
+    file_paths = sorted(file_paths, key=lambda x: (len(x), str.lower(x)))
+    for file_path in file_paths:
+        max_size = 800,800
+        img = Image.open(file_path)
+        img.thumbnail(max_size)
+        files.append(np.array(img))
+    for n in range(1,10):
+        files.append(files[-1])
+    print('generated '+figs_dir+'movie.gif')
+    imageio.mimsave(figs_dir+'movie.gif', files)
+    imageio.mimsave(figs_dir+"/../../"+method[3]+'.gif', files)
+
+def create_model():
+
+    input_shape = (200, 200, 3)
+    model = keras.models.Sequential([
+                keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=input_shape),
+                keras.layers.MaxPooling2D((2, 2)),
+                keras.layers.Conv2D(16, (3, 3), activation="relu"),
+                keras.layers.MaxPooling2D((2, 2)),
+                keras.layers.Flatten(),
+                keras.layers.Dense(128, activation="relu"),
+                keras.layers.Dense(16, activation="softmax"),
+                ])
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=keras.optimizers.Adam(lr=0.001), metrics=["accuracy"])
+    """
+    model = keras.models.Sequential()
+    model.add(keras.layers.Conv2D(10,
+        kernel_size=(3, 3),
+        activation='relu',
+        kernel_initializer=keras.initializers.RandomUniform(),
+        bias_initializer=keras.initializers.RandomUniform(),
+        input_shape=(28,28,1)))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Conv2D(10,
+        kernel_size=(3, 3),
+        activation='relu',
+        kernel_initializer=keras.initializers.RandomUniform(),
+        bias_initializer=keras.initializers.RandomUniform()))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(100,
+        kernel_initializer=keras.initializers.RandomUniform(),
+        bias_initializer=keras.initializers.RandomUniform(),
+        activation='relu'))
+    model.add(keras.layers.Dense(10,
+        kernel_initializer=keras.initializers.RandomUniform(),
+        bias_initializer=keras.initializers.RandomUniform(),
+        activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.SGD(momentum=0.1, lr=0.005),
+        metrics=['accuracy'])"""
+
+    return model
+
+def get_models(models_dir):
+    modelnames = []
+    models = []
+
+    for file_name in os.listdir(models_dir):
+        if file_name.endswith('.h5'):
+            modelnames.append(file_name)
+    modelnames = sorted(modelnames, key=lambda x: (len(x), str.lower(x)))
+
+    for modelname in modelnames:
+        model = keras.models.load_model(models_dir+modelname)
+        models.append((modelname, model))
+
+    models = collections.OrderedDict(models)
+    return (models)
+
+cdict1 = {'red':   ((0.0, 0.0, 0.0),
+                   (0.5, 0.0, 0.1),
+                   (1.0, 1.0, 1.0)),
+
+         'green': ((0.0, 0.0, 0.0),
+                   (1.0, 0.0, 0.0)),
+
+         'blue':  ((0.0, 0.0, 1.0),
+                   (0.5, 0.1, 0.0),
+                   (1.0, 0.0, 0.0))
+         }
+
+blue_red1 = mpl.colors.LinearSegmentedColormap('BlueRed1', cdict1)
+
+methods = [ ("lrp.z",                           {},             ivis.heatmap,    "LRP-Z"),
+            ("lrp.epsilon",                     {"epsilon": 1}, ivis.heatmap,    "LRP-Epsilon"),
+            ("lrp.sequential_preset_a_flat",    {"epsilon": 1}, ivis.heatmap,    "LRP-PresetAFlat"),
+            ("lrp.sequential_preset_b_flat",    {"epsilon": 1}, ivis.heatmap,    "LRP-PresetBFlat"),
+            ("lrp.alpha_2_beta_1",              {},             ivis.heatmap,    "LRP-PresetAlpha2Beta1"),
+            ("lrp.alpha_2_beta_1_IB",           {},             ivis.heatmap,    "LRP-PresetAlpha2Beta1IB"),
+            ("lrp.sequential_preset_a_flat",    {"epsilon": 1}, ivis.heatmap,    "LRP-PresetAFlat"),
+            ("lrp.sequential_preset_b_flat",    {"epsilon": 1}, ivis.heatmap,    "LRP-PresetBFlat")]
+            
+modelfilename = sys.argv[1] if sys.argv[1] != None else "test_model"
+method_n = int(sys.argv[2]) if sys.argv[2] != None else 6
+
+method = methods[method_n]
+print ("Using {}".format(method[3]))
+
+output_nodes = [[0],[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13],[14],[15]]
+analysis_mode = "all"
+models_dir = 'models/'+modelfilename+'/'
+figs_dir = models_dir+'figs/'+method[3]+'/'
+
+model = create_model()
+models = get_models(models_dir)
+
+if not os.path.exists(os.path.dirname(figs_dir)):
+    try:
+        os.makedirs(os.path.dirname(figs_dir))
+    except OSError as exc: # Guard against race condition
+        if exc.errno != errno.EEXIST:
+            raise
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+validation_generator = test_datagen.flow_from_directory(
+        'images/valid/',
+        #target_size=(200, 200),
+        batch_size=32,
+        class_mode='binary',
+        seed= 1)
+
+images = [
+    validation_generator[0][0][0:1],#0
+    validation_generator[0][0][0:1]
+]
+
+print (models)
+# Choosing a test image for the relevance test:
+i = 0
+for image in images:
+    original = iutils.postprocess_images(image)
+    #if analysis_mode == "all":
+    #    original = concat_n_images([original]*len(output_nodes))
+    Image.fromarray(np.uint8(original[0]*255)).save(figs_dir+'fig'+str(i)+"_original.png")
+
+    for modelname in models:
+
+        print("Generating figs for "+modelname+", image "+str(i))
+        model = models[modelname]
+
+        # Stripping the softmax activation from the model
+        model_wo_sm = iutils.keras.graph.model_wo_softmax(model)
+        if analysis_mode == "all":
+            analyzer = innvestigate.create_analyzer(method[0], model_wo_sm, **method[1], neuron_selection_mode="index")
+            lrp_images = [original]
+            predicted = model.predict_classes(image)
+
+            #Generate heatmaps for every class
+            for output_node in output_nodes:
+                analysis = analyzer.analyze(image, output_node)
+                processed_analysis = iutils.postprocess_images(analysis)
+                processed_analysis = method[2](processed_analysis, cmap_type="seismic")
+
+                #Draw a box to identify the predicted class
+                if output_node == predicted:
+                    processed_analysis[0][::2, 0:3, 1] = 0
+                    processed_analysis[0][0:3, ::2, 1] = 0
+                    processed_analysis[0][::2, -4:-1, 1] = 0
+                    processed_analysis[0][-4:-1, ::2, 1] = 0
+
+                lrp_images.append(processed_analysis)
+        else:
+            analyzer = innvestigate.create_analyzer(method[0], model_wo_sm, **method[1])
+            analysis = analyzer.analyze(image)
+            processed_analysis = iutils.postprocess_images(analysis)
+            processed_analysis = method[2](processed_analysis)
+
+            lrp_images = [original, processed_analysis]
+        
+        #Join images and save an overview of the predictions
+        imgs = concat_n_images(lrp_images)
+        Image.fromarray(np.uint8(imgs[0]*255)).save(figs_dir+'fig'+str(i)+"_"+modelname.replace(".h5", "")+"_pred"+str(model.predict_classes(image))+'.png')
+
+    generate_gifs(i, figs_dir, model, image)
     i += 1
 
-exit(0)
+#join_gifs(figs_dir, models, method)
 
+exit(0)
